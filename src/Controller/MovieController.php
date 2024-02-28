343493fce2620dfa2229d7e13a85386112557a14
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-02-28 14:07:12 +0100

    Add fixtures

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,21 +19,8 @@ class MovieController extends AbstractController
     }
 
     #[Route('/{id<\d+>}', name: 'app_movie_show', methods: ['GET'])]
-    public function show(int $id): Response
+    public function show(?Movie $movie): Response
     {
-        $movie = [
-            'id' => $id,
-            'cover' => 'https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg',
-            'title' => 'Star Wars - Episode IV : A New Hope',
-            'plot' => 'A farmer saves the galaxy by blowing up his father\'s toy.',
-            'releasedAt' => new \DateTimeImmutable('25-05-1977'),
-            'genre' => [
-                'Action',
-                'Adventure',
-                'Fantasy',
-            ]
-        ];
-
         return $this->render('movie/show.html.twig', [
             'movie' => $movie,
         ]);
