16d9e4e3accd7db6f944f494fb3b96f0da566f2a
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-02-29 12:24:17 +0100

    Tags

---
+++ src/Notifier/AppNotifier.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Notifier;
+
+use App\Entity\Movie;
+use App\Notifier\Factory\NotificationFactoryInterface;
+use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Recipient\Recipient;
+
+class AppNotifier
+{
+    public function __construct(
+        protected readonly NotifierInterface $notifier,
+        /** @var NotificationFactoryInterface[] $factories */
+        #[TaggedIterator('app.notification_factory', defaultIndexMethod: 'getIndex')]
+        protected iterable $factories
+    )
+    {
+        $this->factories = $factories instanceof \Traversable ? iterator_to_array($factories) : $factories;
+    }
+
+    public function sendMovieNotification(Movie $movie/*, User $user*/): void
+    {
+        $user = $this->getUser();
+        $msg = sprintf("New movie dropped! %s", $movie->getTitle());
+
+        $notification = $this->factories[$user->getPreferredChannel()]->createNotification($msg);
+        $this->notifier->send($notification, new Recipient($user->getEmail()));
+    }
+
+    private function getUser(): object
+    {
+        return new class {
+            public function getEmail(): string
+            {
+                return 'me@me.com';
+            }
+
+            public function getPreferredChannel(): string
+            {
+                return 'slack';
+            }
+        };
+    }
+}
