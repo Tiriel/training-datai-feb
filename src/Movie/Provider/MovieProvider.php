db24718ed0e8d04eef4afe188e2faf64afa3d665
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-02-29 17:21:06 +0100

    Console

--- src/Movie/Provider/MovieProvider.php
+++ src/Movie/Provider/MovieProvider.php
@@ -7,9 +7,12 @@ use App\Movie\Consumer\OmdbApiConsumer;
 use App\Movie\Enum\SearchType;
 use App\Movie\Transformer\OmdbToMovieTransformer;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class MovieProvider
 {
+    protected ?SymfonyStyle $io = null;
+
     public function __construct(
         protected readonly EntityManagerInterface $em,
         protected readonly OmdbApiConsumer $consumer,
@@ -19,13 +22,22 @@ class MovieProvider
     {
     }
 
+    public function setIo(?SymfonyStyle $io): void
+    {
+        $this->io = $io;
+    }
+
     public function getOne(SearchType $type, string $value): Movie
     {
         if (($movie = $this->em->getRepository(Movie::class)->findLikeOmdb($type, $value)) instanceof Movie) {
+            $this->io?->note('Movie already in database!');
+
             return $movie;
         }
 
+        $this->io?->text('Searching on OMDb API...');
         $data = $this->consumer->getMovieData($type, $value);
+        $this->io?->text('Movie found.');
         $movie = $this->transformer->fromOmdb($data);
 
         foreach ($this->genreProvider->parseOmdbString($data['Genre']) as $genre) {
@@ -34,6 +46,7 @@ class MovieProvider
 
         $this->em->persist($movie);
         $this->em->flush();
+        $this->io->note('Movie saved in database!');
 
         return $movie;
     }
